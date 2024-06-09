d223587812b78843b698c5afbcd4acc3ebfc0b7e
Pasha Rezero <kivachuk.2018@gmail.com>
2024-06-09 17:55:38 +0300

    add show main build only if connected socket

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import House from "./components/Structure/House/House";
 import Map from "./components/Map/Map";
 import socket from "./socket.io";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 function App() {
+  const [connected, setConnected] = useState(false);
   useEffect(() => {
     socket.emit("message", "Hello from client!");
 
     socket.on("message", (msg) => {
       console.log("Received message from server:", msg);
+    });
+
+    socket.on("connect", () => {
+      setTimeout(() => {
+        setConnected(true);
+      }, 2000);
     });
 
     if (socket === undefined) return () => socket.disconnect();
@@ -17,7 +24,7 @@ function App() {
     <div className="App container">
       <p>Набросок карты</p>
       <Map width={30} height={30} />
-      <House />
+      {connected && <House />}
     </div>
   );
 }
